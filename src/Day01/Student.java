1293c54354e47ee4acbd0e2d4af57a60ba7643d4
HavvaG <k_hasret@hotmail.com>
2023-01-30 17:20:35 +0100

    task 3 yapildi

--- src/Day01/Student.java
+++ src/Day01/Student.java
@@ -3,6 +3,6 @@ package Day01;
 public class Student {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
-
+        System.out.println("Github a yollandi");
     }
 }
